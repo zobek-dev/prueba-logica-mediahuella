07c364fc06972b93d610d7c6772ebb948a278049
zobek-dev <sebastian@mediahuella.com>
2024-05-11 00:49:10 -0400

    prueba logica

--- main.js
+++ main.js
@@ -3,37 +3,21 @@ Siga las instrucciones de cada enunciado y recuerde que la funcion debe llevar u
 ¡Buena suerte!
 **************************************************************************************************************/ 
 
-//1. La función es Palindromo debe retornar true si un string es palíndromo y false si no lo es. es una palabra o frase que se lee igual en un sentido que en otro1​ (por ejemplo; "ana", "anna", "otto", "anita lava la tina")
+//1. La función esPalindromo debe retornar true si un string es palíndromo y false si no lo es. es una palabra o frase que se lee igual en un sentido que en otro1​ (por ejemplo; "ana", "anna", "otto", "anita lava la tina")
 function esPalindromo(str){
   // Escribe el código aquí
-  const arr = Array.from(str.trim()) //puede ser arr.split("")
-  const reverse= arr.filter(el => el != ' ').reverse().join('') //quitamos los espacios en blanco e invertimos la cadena
-  return str.replace(/\s+/g, '') === reverse
 }
 
 //2. Dado un string, la funcion contadorPalabras debe retornar la cantidad de palabras que contiene el string que ingresa. Ej: "Hola soy goku" = 3, "   Hola tengo 3 años de experiencia en javascript      " = 8, "  El lenguaje C    es      un lenguaje   compilado     " = 7
 
 function contadorPalabras(str){
   // Escribe el codigo aquí
-  const arr = str.replace(/\s+/g, ' ').trim().split(' ')
-  return arr.length
 } 
 
+//3. Dado un string y un caracter, generar la funcion caracter repetido la cual debe retornar la cuenta de caracteres repetidos del string. Ej:  "aBssdfTassdaaaaffdf" y "a" es 6
 function caracterRepetido(string, char){
-  //Escribe el codigo aqui
-  let count = 0
-  if(char.length > 1){
-    return count
-  }
-  const arr = string.split('')
-  arr.forEach(el => {
-    if(el === char) count += 1 
-  })
-  return count
+  //Escribe el codigo aquí
 } 
 
-console.log(caracterRepetido('aBssdfTassdaaaaffdf', 'a'))
-console.log(caracterRepetido('aBssdfTassdaaaaffdf', 'B'))
-console.log(caracterRepetido('aBssdfTassdaaaaffdf', 'aB'))
  
 module.exports = { esPalindromo, contadorPalabras, caracterRepetido };
